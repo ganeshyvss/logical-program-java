46fc5deb9079f6be77882a9fed25ec063a41c6ba
ganeshyvss <95574832+ganeshyvss@users.noreply.github.com>
2022-02-26 15:47:10 +0530

    Add files via upload

---
+++ Arrayfind.java
@@ -0,0 +1,16 @@
+class Arrayfind{
+    
+    public static void main(String args[]){
+      
+      int arr[]={23,45,67,78,90,65,33,22};
+      
+      System.out.println(arr[4]);
+      System.out.println(arr[2]);
+      
+      System.out.println(arr.length);  
+      
+      System.out.println(arr[arr.length-1]);
+      
+           
+    }
+}
---
+++ Arraylength.java
@@ -0,0 +1,13 @@
+
+class Arraylength{
+    
+    public static void main(String args[]){
+      
+      int arr[]={23,45,67,78,90,65,33,22};
+      
+      for(int i=0;i<arr.length;i++){
+          
+          System.out.print(arr[i]+"  ");
+      }
+    }
+}
